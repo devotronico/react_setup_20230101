48a8798a9122d9a6f4bdfc0faa1d346ea4152078
redeluni <dmanzi83@gmail.com>
2022-12-30 21:51:49 +0100

    commit 5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>hello World</h1>
       <Link to='/'>Home</Link> | <Link to='/about'>About</Link>
     </div>
   );
