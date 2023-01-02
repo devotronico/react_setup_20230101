8fab4be37d7e48a2645439722fe1048b398c1c5d
redeluni <dmanzi83@gmail.com>
2023-01-02 18:37:15 +0100

    feat(all-home): set rules for the scope

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,12 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const prefixes = ['all', 'web', 'mobile'];
+const scopes = ['home', 'about', 'contact', 'news'];
+const arr = scopes.map(s => prefixes.map(p => `${p}-${s}`));
+
+const prefixed = arr.flat();
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', prefixed],
+  },
+};
