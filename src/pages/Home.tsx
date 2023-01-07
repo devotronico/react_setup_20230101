410ba059db15a2048b171a800cecfdbade628d8d
redeluni <dmanzi83@gmail.com>
2023-01-07 15:12:57 +0100

    fix(all-home): add the date

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
     <div>
       <h1>Hello World</h1>
       <Link to='/'>Home</Link> | <Link to='/about'>About</Link> | <Link to='/contact'>Contact</Link>
-      <p>sito creato da Daniele</p>
+      <p>sito creato da Daniele 2022</p>
     </div>
   );
 }
